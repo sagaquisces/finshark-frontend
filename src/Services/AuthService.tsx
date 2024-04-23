865547ce0b3473408bc2f9b64ba5397353027846
Michael Dunlap <michaeldunlap@earthlink.net>
2024-04-22 17:59:11 -0800

    back to https for backend

--- src/Services/AuthService.tsx
+++ src/Services/AuthService.tsx
@@ -9,7 +9,7 @@ console.log(import.meta.env.DEV)
 console.log(import.meta.env.PROD)
 
 if(import.meta.env.PROD) {
-  api = "http://finshark-api.onrender.com:8080/api/"
+  api = "https://finshark-api.onrender.com:8080/api/"
 } else {
   api = "http://localhost:5094/api/"
 }
