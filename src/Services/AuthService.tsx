79906057c7732661ec0f9e8f60b1649f16e921ad
Michael Dunlap <michaeldunlap@earthlink.net>
2024-05-18 14:04:07 -0800

    adding https for prod api

--- src/Services/AuthService.tsx
+++ src/Services/AuthService.tsx
@@ -9,7 +9,7 @@ console.log(import.meta.env.DEV)
 console.log(import.meta.env.PROD)
 
 if(import.meta.env.PROD) {
-  api = "http://finshark-api-1ioo:8080/"
+  api = "https://finshark-api.onrender.com/api/"
 } else {
   api = "http://localhost:5094/api/"
 }
